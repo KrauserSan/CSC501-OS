44c023e38e3ab4698715051b01de24949cbc7756
KrauserSan <vpahwa@bn20-161.dcs.mcnc.org>
2017-09-01 01:54:27 -0400

    added zfunction.S

---
+++ pa0/sys/main.c
@@ -0,0 +1,18 @@
+/* user.c - main */
+
+#include <conf.h>
+#include <kernel.h>
+#include <proc.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ *  main  --  user main program
+ *------------------------------------------------------------------------
+ */
+int main()
+{
+	kprintf("\n\nHello World, Xinu lives\n\n");
+	kprintf("yolo:\n");
+	kprintf("zfunction(0xaabbccdd) = 0x%lX\n",zfunction(0xaabbccdd));
+	return 0;
+}
