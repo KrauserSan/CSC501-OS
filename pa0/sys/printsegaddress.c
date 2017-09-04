d1df73718258d88c00a4659804f3d9081dd6fc7c
KrauserSan <vpahwa@bn17-40.dcs.mcnc.org>
2017-09-03 20:33:55 -0400

    modified files

--- pa0/sys/printsegaddress.c
+++ pa0/sys/printsegaddress.c
@@ -7,11 +7,11 @@ extern long end;
 void printsegaddress(){
 
 
-	kprintf("Current: etext[0x%lx] = 0x%lx, edata[0x%lx]=0x%lx, ebss[0x%lx]=0x%lx \n",
+	kprintf("Current: etext[0x%08X] = 0x%08X, edata[0x%08X]=0x%08X, ebss[0x%08X]=0x%08X \n",
 		&etext-1,*(&etext-1),&edata-1,*(&edata-1),&end-1,*(&end-1));
-	kprintf("Preceding: etext[0x%lx] = 0x%lx, edata[0x%lx]=0x%lx, ebss[0x%lx]=0x%lx \n",
+	kprintf("Preceding: etext[0x%08X] = 0x%08X, edata[0x%08X]=0x%08X, ebss[0x%08X]=0x%08X \n",
 		&etext-2,*(&etext-2),&edata-2,*(&edata-2),&end-2,*(&end-2));
-	kprintf("After: etext[0x%lx] = 0x%lx, edata[0x%lx]=0x%lx, ebss[0x%lx]=0x%lx \n",
+	kprintf("After: etext[0x%08X] = 0x%08X, edata[0x%08X]=0x%08X, ebss[0x%08X]=0x%08X \n",
 		&etext,*(&etext),&edata,*(&edata),&end,*(&end));
     
 
--- pa0/sys/printtos.c
+++ pa0/sys/printtos.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 
-/*
-unsigned long *stackPointer;
-unsigned long *basePointer;
+
+unsigned long *esp;
+unsigned long *ebp;
 
 void printtos(){
-    asm("movl %esp, stackPointer");
-    asm("movl %ebp, basePointer");
-    kprintf("Before[0x%x]: 0x%x\n", basePointer+2, *(basePointer+2));
-    kprintf("After[0x%x]: 0x%x\n", stackPointer, *stackPointer);
-    
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    int d = 4;
-    kprintf("    element[0x%x]: 0x%x, var:0x%x\n", stackPointer, *stackPointer,a);
-    kprintf("    element[0x%x]: 0x%x, var:0x%x\n", stackPointer, *(stackPointer+5),b);
-    kprintf("    element[0x%x]: 0x%x, var:0x%x\n", stackPointer, *(stackPointer+4),c);
-    kprintf("    element[0x%x]: 0x%x, var:0x%x\n", stackPointer, *(stackPointer+3),d);
-*/
+    asm("movl %esp, esp");
+    asm("movl %ebp, ebp");
+    kprintf("Before[0x%08X]: 0x%08X\n", ebp+2, *(ebp+2));
+    kprintf("After[0x%08X]: 0x%08X\n", esp, *esp);
+  
+    kprintf("    element[0x%08X]: 0x%08X\n", 
+		esp+1, *(esp+1));
+    kprintf("    element[0x%08X]: 0x%08X\n", 
+		esp+2, *(esp+2));
+    kprintf("    element[0x%08X]: 0x%08X\n", 
+		esp+3, *(esp+3));
+    kprintf("    element[0x%08X]: 0x%08X\n", 
+		esp+4, *(esp+4));
 
-unsigned long *x1, *x2;
-
-void printtos(){
-	asm("movl %esp, x1");
-	asm("movl %ebp, x2");
-	kprintf("ebp:\t 0x%x \nesp_before_entry:\t 0x%x \n",x2,x2+2);
-	kprintf("esp:\t 0x%x \n",x1);
-	int i;
-	for(i=0; i<6;i++)
-		kprintf("esp+%d:\t 0x%x [esp+%d]:\t 0x%x\n",i,x1+i,i,*(x1+i));
-	kprintf("--------------------------------\n");  
 }
