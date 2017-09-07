ea53973c4aa3d6b9cfc928931ef23e718faac852
KrauserSan <vpahwa@vclv13-243.hpc.ncsu.edu>
2017-09-07 15:11:47 -0400

    modified printtos

--- pa0/sys/printtos.c
+++ pa0/sys/printtos.c
@@ -5,14 +5,12 @@ unsigned long *esp;
 unsigned long *ebp;
 
 void printtos(){
-    asm("movl %esp, esp");
     asm("movl %ebp, ebp");
     kprintf("Before[0x%08X]: 0x%08X\n", (ebp+2), *(ebp+2));
     kprintf("After[0x%08X]: 0x%08X\n", ebp, *ebp);
-  
-    int a = 0x0001;
-    int b = 0x0002;
-    int c = 0x0003;
+    int a = 0x1;
+    int b = 0x2;
+    int c = 0x3;
     asm("movl %esp, esp");
     kprintf("    element[0x%08X]: 0x%08X\n", esp, *esp);
     kprintf("    element[0x%08X]: 0x%08X\n", esp+1, *(esp+1));
